0f7c4432dcae4b7eb7aa7352cd9855dca6df355f
Ning Ren <ninren@student.ethz.ch>
2022-12-16 00:35:19 +0100

    wavelet basis

--- wavelet1d.py
+++ wavelet1d.py
@@ -78,15 +78,18 @@ class PrimalMRA:
 
         self.ML = solve_triangular(B2, B1)
 
-    def refinement_matrix(self, j):
+    def compute_A(self, j):
         d = self.d
         A = np.zeros((2**(j+1) - d + 1, 2**j - d + 1))
         for k in range(2**j - d + 1):
             A[2*k:2*k+d+1, k] = self.sf.refinement_coeffs()
+        return A
 
+    def refinement_matrix(self, j):
+        d = self.d
         M0 = np.zeros((2**(j+1) + d - 1, 2**j + d - 1))
         M0[:2*d-2, :d-1] = self.ML
-        M0[d-1:2**(j+1), d-1:2**j] = A
+        M0[d-1:2**(j+1), d-1:2**j] = self.compute_A(j)
         M0[2-2*d:, 1-d:] = self.ML[::-1, ::-1]
 
         return 1 / np.sqrt(2) * M0
@@ -205,19 +208,19 @@ class DualMRA:
                 D2[n, k] = binom(n, k) * k0**(n-k) * (-1)**k
                 D3[n, k] = factorial(k)\
                     * divided_diff(lambda x: x**n, np.arange(k + 1))
-        D_t = (D1 @ D2 @ D3)[:, ::-1]
-        block1 = np.empty((d + 3 * d_t - 3, d_t))
-        block1[:d_t, :] = \
-            D_t.T @ np.diag([2**(-r) for r in range(d_t)])
-        block1[d_t:, :] = np.array(
-            [[beta(n-l1_t, r) for r in range(d_t)] for n in range(d+2*d_t-3)]
-        )
-        ML_t[d-2:, d-2:] = block1 @ np.linalg.inv(D_t.T)
+        D_t = D1 @ D2 @ D3
+        rhs = np.empty((d_t, d + 3 * d_t - 3))
+        rhs[:, :d_t] = \
+            np.diag([2**(-r) for r in range(d_t)]) @ D_t[:, ::-1]
+        rhs[:, d_t:] = np.array(
+            [[beta(n-l1_t, r) for n in range(d + 2 * d_t - 3)]
+             for r in range(d_t)])
+        ML_t[d-2:, d-2:] = solve_triangular(D_t, rhs, lower=True)[::-1, :].T
 
         def compute_gramian():
-            n = ML_full.shape[1]
-            UL = ML_full[:n, :]
-            LL = ML_full[n:, :]
+            n = ML.shape[1]
+            UL = ML[:n, :]
+            LL = ML[n:, :]
             UL_t = ML_t[:n, :]
             LL_t = ML_t[n:, :]
             lhs = 2 * np.identity(n**2) - np.kron(UL_t.T, UL.T)
@@ -228,30 +231,198 @@ class DualMRA:
         gramian_full = np.identity(2 * d + 3 * d_t - 5)
         for k in range(d - 3, -1, -1):
             gramian_full[:d+d_t-2, :d+d_t-2] = compute_gramian()
-            B_k = ML_full[:, :k+d].T @ gramian_full[:, k+1:2*k+d+1] / 2.
+            B_k = ML[:, :k+d].T @ gramian_full[:, k+1:2*k+d+1] / 2.
 
-            delta = np.zeros(k+d)
+            delta = np.zeros(k + d)
             delta[k] = 1
             ML_t[k+1:2*k+d+1, k] = np.linalg.solve(B_k, delta)
-
-        # Biorthogonalization
-
-        gramian = compute_gramian()
-        ML_t[:d+d_t-2, :d+d_t-2] = gramian @ ML_t[:d+d_t-2, :d+d_t-2]
-        ML_t = ML_t @ np.linalg.inv(gramian)
         ML_t[np.abs(ML_t) < 1e-9] = 0.
+
+        # Biorthogonalize
+
+        gramian = np.triu(compute_gramian())
+        gramian[:d-2, :d-2] = np.identity(d - 2)
+        ML_t[:d+d_t-2, :d+d_t-2] = gramian @ ML_t[:d+d_t-2, :d+d_t-2]
+        ML_t = solve_triangular(gramian.T, ML_t.T, lower=True).T
+
+        """
+        # Apply boundary condition
+        UL = ML_t[:d+d_t-2, :d+d_t-2]
+        w, v = np.linalg.eig(UL.T)
+        idx = np.argmax(w)
+        assert np.isclose(w[idx], 1.)
+        la = v[:, idx]
+        la /= la[0]
+        """
+
         self.ML = ML_t
 
-    def refinement_matrix(self, j):
+    def compute_A(self, j):
+        assert j >= self.j0
         d = self.d
         d_t = self.d_t
         A_t = np.zeros((2**(j+1) - d - 2 * d_t + 3, 2**j - d - 2 * d_t + 3))
         for k in range(2**j - d - 2 * d_t + 3):
             A_t[2*k:2*k+d+2*d_t-1, k] = self.sf_t.refinement_coeffs()
+        return A_t
 
+    def refinement_matrix(self, j):
+        d = self.d
+        d_t = self.d_t
         M0_t = np.zeros((2**(j+1) + d - 1, 2**j + d - 1))
         M0_t[:2*d+3*d_t-5, :d+d_t-2] = self.ML
-        M0_t[d+d_t-2:2**(j+1)-d_t+1, d+d_t-2:2**j-d_t+1] = A_t
+        M0_t[d+d_t-2:2**(j+1)-d_t+1, d+d_t-2:2**j-d_t+1] = self.compute_A(j)
         M0_t[-(2*d+3*d_t-5):, -(d+d_t-2):] = self.ML[::-1, ::-1]
 
         return 1 / np.sqrt(2) * M0_t
+
+
+class WaveletBasis:
+    def __init__(self, d, d_t):
+        self.d = d
+        self.d_t = d_t
+        self.sf = PrimalScalingFunction(d)
+        self.sf_t = DualScalingFunction(d, d_t)
+        self.mra = PrimalMRA(d)
+        self.mra_t = DualMRA(d, d_t)
+
+    def basis_functions(self, j, nu=0):
+        d = self.d
+        n = 2**j
+        bs = []
+
+        def bspline(knots, coeffs, d):
+            b = BSpline(knots, coeffs, d - 1, extrapolate=False)
+            if nu > 0:
+                b = b.derivative(nu)
+            return lambda x: np.nan_to_num(b(x))
+
+        _, M1, _, _ = self.refinement_matrix(j)
+        knots = np.concatenate((np.zeros(d - 1),
+                                np.linspace(0, 1, 2**(j+1) + 1),
+                                np.ones(d - 1)))
+        for k in range(n):
+            coeffs = 2**((j+1)/2) * M1[:, k]
+            bs.append(bspline(knots, coeffs, d))
+        return bs
+
+    def support(self, j, k):
+        pass
+
+    def plot(self, j, k=None, nu=0):
+        bs = self.basis_functions(j, nu)
+        x = np.linspace(0, 1, 1000)
+
+        if k is None:
+            for b in bs:
+                plt.plot(x, b(x))
+        else:
+            plt.plot(x, bs[k](x))
+        plt.show()
+
+    def initial_completion(self, j):
+        d = self.d
+        l1 = self.sf.l1
+        l2 = self.sf.l2
+        p = 2**j - d + 1
+        q = 2**(j+1) - d + 1
+
+        ML = self.mra.ML
+        P = np.identity(q + 2 * d - 2)
+        P[:2*d-2, :d-1] = ML
+        P[2-2*d:, 1-d:] = ML[::-1, ::-1]
+
+        ML_inv = np.linalg.inv(P[:2*d-2, :2*d-2])
+        P_inv = np.identity(q + 2 * d - 2)
+        P_inv[:2*d-2, :2*d-2] = ML_inv
+        P_inv[2-2*d:, 2-2*d:] = ML_inv[::-1, ::-1]
+
+        A = self.mra.compute_A(j)
+        H = np.identity(q)
+        H_inv = np.identity(q)
+        for i in range(d):
+            if i % 2 == 0:
+                m = i // 2
+                v = A[m, 0] / A[m+1, 0]
+                H_i = np.identity(q)
+                H_i_inv = np.identity(q)
+                rows = np.arange(m % 2, q, 2)
+                cols = np.arange(m % 2 + 1, q, 2)
+                rows = rows[:cols.size]
+                H_i[rows, cols] = -v
+                H_i_inv[rows, cols] = v
+            else:
+                m = (i - 1) // 2
+                v = A[d-m, 0] / A[d-m-1, 0]
+                H_i = np.identity(q)
+                H_i_inv = np.identity(q)
+                rows = np.arange(q - m % 2, 0, -2) - 1
+                cols = np.arange(q - m % 2 - 1, 0, -2) - 1
+                rows = rows[:cols.size]
+                H_i[rows, cols] = -v
+                H_i_inv[rows, cols] = v
+            A = H_i @ A
+            H = H_i @ H
+            H_inv = H_inv @ H_i_inv
+
+        b = A[l2, 0]
+        rows = np.arange(p) * 2 + l2
+        cols = np.arange(p)
+        A = np.zeros_like(A)
+        A[rows, cols] = b
+        B = np.zeros_like(A.T)
+        B[cols, rows] = 1 / b
+        F = np.zeros_like(A)
+        rows -= 1
+        F[rows, cols] = 1
+
+        A_hat = np.zeros((q + 2*d - 2, p + 2 * d - 2))
+        A_hat[:d-1, :d-1] = np.identity(d - 1)
+        A_hat[d-1:q+d-1, d-1:p+d-1] = A
+        A_hat[1-d:, 1-d:] = np.identity(d - 1)
+
+        B_hat = np.zeros_like(A_hat.T)
+        B_hat[:d-1, :d-1] = np.identity(d - 1)
+        B_hat[d-1:p+d-1, d-1:q+d-1] = B
+        B_hat[1-d:, 1-d:] = np.identity(d - 1)
+
+        F_hat = np.zeros((q + 2 * d - 2, p + d - 1))
+        F_hat[d-1:l2+d-2, :l2-1] = np.identity(l2 - 1)
+        F_hat[d-1:q+d-1, l2-1:p+l2-1] = F
+        F_hat[l1-d+1:1-d, l1:] = np.identity(-l1)
+
+        H_hat = np.identity(q + 2 * d - 2)
+        H_hat[d-1:1-d, d-1:1-d] = H
+        H_hat_inv = np.identity(q + 2 * d - 2)
+        H_hat_inv[d-1:1-d, d-1:1-d] = H_inv
+
+        M0 = self.mra.refinement_matrix(j)
+        M1 = 1 / np.sqrt(2) * P @ H_hat_inv @ F_hat
+        G0 = np.sqrt(2) * B_hat @ H_hat @ P_inv
+        G1 = np.sqrt(2) * F_hat.T @ H_hat @ P_inv
+        return M0, M1, G0, G1
+
+    def refinement_matrix(self, j):
+        M0, M1, _, G1 = self.initial_completion(j)
+        M0_t = self.mra_t.refinement_matrix(j)
+        M1 = M1 - M0 @ M0_t.T @ M1
+
+        # Symmetrize
+        N1 = M1[:, :2**(j-1)]
+        N1_t = G1.T[:, :2**(j-1)]
+        H1 = np.hstack((N1, N1[::-1, ::-1]))
+        H1_t = np.hstack((N1_t, N1_t[::-1, ::-1]))
+        M1 = np.linalg.solve(H1.T @ H1_t, H1.T).T
+        M1_t = H1_t
+
+        M0[np.abs(M0) < 1e-9] = 0.
+        M1[np.abs(M1) < 1e-9] = 0.
+        M0_t[np.abs(M0_t) < 1e-9] = 0.
+        M1_t[np.abs(M1_t) < 1e-9] = 0.
+        return M0, M1, M0_t, M1_t
+
+    def gramian(self, j):
+        pass
+
+    def inner_product(self, j, nu=0):
+        pass
